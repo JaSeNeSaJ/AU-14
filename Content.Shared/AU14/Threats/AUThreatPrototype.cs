28bdc86eb659f6d7d8211c8c191cc69089993e2d
Admin <iamevergreendev@gmail.com>
2026-04-14 18:29:01 -0600

    third party interval adjustment

--- Content.Shared/AU14/Threats/AUThreatPrototype.cs
+++ Content.Shared/AU14/Threats/AUThreatPrototype.cs
@@ -78,7 +78,7 @@ public sealed partial class ThreatPrototype : IPrototype
 
     [DataField("thirdpartyinterval", required: false)]
 
-    public int ThirdPartyInterval { get; private set; } = 24000;
+    public int ThirdPartyInterval { get; private set; } = 17500;
 
     [DataField("lorePrimer")]
     public ProtoId<LorePrimerPrototype>? LorePrimer { get; private set; }
