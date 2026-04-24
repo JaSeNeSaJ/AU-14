c44c8f41234acf54ac467758b671b3f11a23faab
TheHellFireo <ussfkamurof55@gmail.com>
2026-04-24 12:20:56 +0300

    Left over from testing im guessing?

--- Content.Shared/_RMC14/Rules/CMDistressSignalRuleComponent.cs
+++ Content.Shared/_RMC14/Rules/CMDistressSignalRuleComponent.cs
@@ -197,7 +197,7 @@ public sealed partial class CMDistressSignalRuleComponent : Component
     public bool DoJobSlotScaling = true;
 
     [DataField]
-    public bool AutoEnd = false;
+    public bool AutoEnd = true;
 
     [DataField]
     public bool StartARESAnnouncements = true;
