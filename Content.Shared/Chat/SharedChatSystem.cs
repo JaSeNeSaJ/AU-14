79dad31f743778c047b3f21eb74f9782a2711ff2
TheHellFireo <ussfkamurof55@gmail.com>
2026-05-13 04:20:25 +0300

    Adds the Predator and Abomination

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections.Frozen;
 using System.Text.RegularExpressions;
+using Content.Shared._CMU14.Yautja;
 using Content.Shared._RMC14.Chat;
 using Content.Shared.AU14;
 using Content.Shared._RMC14.Xenonids;
@@ -144,7 +145,7 @@ public abstract class SharedChatSystem : EntitySystem
         if (input.StartsWith(RadioCommonPrefix))
         {
             output = SanitizeMessageCapital(input[1..].TrimStart());
-            channel = (HasComp<XenoComponent>(source) || HasComp<CultistComponent>(source))
+            channel = ((HasComp<XenoComponent>(source) && !IsHivebrokenXeno(source)) || HasComp<CultistComponent>(source))
                 ? _prototypeManager.Index<RadioChannelPrototype>(HivemindChannel)
                 : _prototypeManager.Index<RadioChannelPrototype>(CommonChannel);
 
@@ -167,7 +168,7 @@ public abstract class SharedChatSystem : EntitySystem
         if (input.Length < 2 || char.IsWhiteSpace(input[1]))
         {
             output = SanitizeMessageCapital(input[1..].TrimStart());
-            if (HasComp<XenoComponent>(source))
+            if (HasComp<XenoComponent>(source) && !IsHivebrokenXeno(source))
                 return false;
 
             if (!quiet)
@@ -209,10 +210,15 @@ public abstract class SharedChatSystem : EntitySystem
         RaiseLocalEvent(source, ref prefixEv);
         channel = prefixEv.Channel;
 
-        if (HasComp<XenoComponent>(source) && channel == null)
+        if (HasComp<XenoComponent>(source) && !IsHivebrokenXeno(source) && channel == null)
             return false;
 
         return true;
+    }
+
+    private bool IsHivebrokenXeno(EntityUid uid)
+    {
+        return TryComp(uid, out YautjaThrallComponent? thrall) && thrall.Hivebroken;
     }
 
     public string SanitizeMessageCapital(string message)
