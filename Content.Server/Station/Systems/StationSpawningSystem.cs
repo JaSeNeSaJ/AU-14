14a0ab745e7c8c7b9689ce3f8c1b46e0933ba0cc
Admin <iamevergreendev@gmail.com>
2026-05-23 15:44:31 -0600

    Alliance and withdraw consoles

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -509,6 +509,8 @@ public sealed partial class StationSpawningSystem : SharedStationSpawningSystem
                 {
                     _npcFaction.AddFaction((entity.Value, CompOrNull<NpcFactionMemberComponent>(entity.Value)), addFaction);
                 }
+                if (selectedPlatoon.NpcFaction is { } platoonNpcFaction)
+                    _npcFaction.AddFaction((entity.Value, CompOrNull<NpcFactionMemberComponent>(entity.Value)), platoonNpcFaction);
             }
         }
         return entity.Value;
