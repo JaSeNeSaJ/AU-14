590b7645a5aab978ba44078b6c4d415f94aea8e8
MACMAN2003 <macman2003c@gmail.com>
2026-05-26 14:50:33 -0700

    Create GeneratedReagentData.cs

---
+++ Content.Shared/_AU14/Chemistry/Reagents/GeneratedReagentData.cs
@@ -0,0 +1,11 @@
+//pretty please license this under the MIT license :) - MACMAN2003
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Content.Shared._AU14.Chemistry.Reagents;
+
+public sealed partial class GeneratedReagentData
+{
+
+}
