fa6e37d4028686a34f33c1fb2d642d4c10f7af8b
TheHellFireo <ussfkamurof55@gmail.com>
2026-06-07 15:59:28 +0300

    Improve admin log message formatting

--- Content.Client/Administration/UI/CustomControls/AdminLogLabel.cs
+++ Content.Client/Administration/UI/CustomControls/AdminLogLabel.cs
@@ -11,6 +11,10 @@ public sealed class AdminLogLabel : PanelContainer
 {
     private static readonly Color BackgroundColor = Color.FromHex("#1B1B1E");
     private static readonly Color BorderColor = Color.FromHex("#303645");
+    private static readonly Color ChipBackgroundColor = Color.FromHex("#23272F");
+    private static readonly Color ChipBorderColor = Color.FromHex("#343A45");
+    private static readonly Color ChipLabelColor = Color.FromHex("#8E98A8");
+    private static readonly Color ChipValueColor = Color.FromHex("#DCE5F2");
     private static readonly Color HeaderColor = Color.FromHex("#C8D0DB");
     private static readonly Color SeparatorColor = Color.FromHex("#252A33");
     private static readonly Color TypeColor = Color.FromHex("#9DBCE6");
@@ -19,6 +23,7 @@ public sealed class AdminLogLabel : PanelContainer
     {
         Log = log;
         Separator = separator;
+        Details = AdminLogMessageFormatter.GetDetails(log.Type, log.Message);
         TimeText = $"{log.Date:HH:mm:ss}";
         ImpactText = log.Impact.ToString();
         TypeText = log.Type.ToString();
@@ -75,10 +80,15 @@ public sealed class AdminLogLabel : PanelContainer
         {
             HorizontalExpand = true,
         };
-        message.SetMessage(log.Message);
+        message.SetMessage(AdminLogMessageFormatter.Format(log.Type, log.Message));
 
         root.AddChild(header);
         root.AddChild(message);
+
+        if (Details.Count > 0)
+        {
+            root.AddChild(CreateDetailsRow(Details));
+        }
 
         AddChild(root);
 
@@ -89,6 +99,8 @@ public sealed class AdminLogLabel : PanelContainer
     public new SharedAdminLog Log { get; }
 
     public HSeparator Separator { get; }
+
+    public IReadOnlyList<AdminLogMessageDetail> Details { get; }
 
     public string TimeText { get; }
 
@@ -105,6 +117,69 @@ public sealed class AdminLogLabel : PanelContainer
             LogImpact.Medium => Color.FromHex("#D7B95E"),
             LogImpact.Low => Color.FromHex("#8DBA75"),
             _ => BorderColor,
+        };
+    }
+
+    private static Control CreateDetailsRow(IReadOnlyList<AdminLogMessageDetail> details)
+    {
+        var row = new BoxContainer
+        {
+            Orientation = BoxContainer.LayoutOrientation.Horizontal,
+            HorizontalExpand = true,
+            SeparationOverride = 4,
+        };
+
+        foreach (var detail in details)
+        {
+            row.AddChild(CreateDetailChip(detail));
+        }
+
+        return row;
+    }
+
+    private static Control CreateDetailChip(AdminLogMessageDetail detail)
+    {
+        var chip = new PanelContainer
+        {
+            PanelOverride = new StyleBoxFlat
+            {
+                BackgroundColor = ChipBackgroundColor,
+                BorderColor = ChipBorderColor,
+                BorderThickness = new Thickness(1),
+                ContentMarginLeftOverride = 6,
+                ContentMarginRightOverride = 6,
+                ContentMarginTopOverride = 2,
+                ContentMarginBottomOverride = 2,
+            },
+        };
+
+        var text = new RichTextLabel
+        {
+            MaxWidth = 220,
+        };
+
+        var message = new Robust.Shared.Utility.FormattedMessage();
+        message.PushColor(ChipLabelColor);
+        message.AddText($"{detail.Label} ");
+        message.Pop();
+        message.PushColor(GetDetailValueColor(detail.Label));
+        message.AddText(detail.Value);
+        message.Pop();
+        text.SetMessage(message);
+
+        chip.AddChild(text);
+        return chip;
+    }
+
+    private static Color GetDetailValueColor(string label)
+    {
+        return label switch
+        {
+            "damage" => AdminLogMessageFormatter.DamageColor,
+            "proto" => AdminLogMessageFormatter.PrototypeColor,
+            "coord" => AdminLogMessageFormatter.CoordinateColor,
+            "command" => AdminLogMessageFormatter.MessageColor,
+            _ => ChipValueColor,
         };
     }
 
