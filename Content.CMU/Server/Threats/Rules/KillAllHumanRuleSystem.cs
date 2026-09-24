a7259d59d7a03fb17d566afb529472250d7e5393
TheHellFireo <ussfkamurof55@gmail.com>
2026-09-24 03:09:59 +0300

    Fix reported colony loadouts, combat, reinforcements, and hive behavior

--- Content.CMU/Server/Threats/Rules/KillAllHumanRuleSystem.cs
+++ Content.CMU/Server/Threats/Rules/KillAllHumanRuleSystem.cs
@@ -111,10 +111,10 @@ public sealed partial class KillAllHumanRuleSystem : GameRuleSystem<KillAllHuman
                 continue;
             }
 
-            if (crashedDropship && _rmcPlanet.IsOnPlanet(Transform(uid)) && mobState.CurrentState != MobState.Dead)
+            if (crashedDropship && _rmcPlanet.IsOnPlanet(Transform(uid)) && !_threatRuleHelper.IsEliminated(uid, mobState))
                 continue;
 
-            if (mobState.CurrentState == MobState.Dead)
+            if (_threatRuleHelper.IsEliminated(uid, mobState))
                 eliminated++;
 
             else if (HasPrisonJumpsuit(uid)
